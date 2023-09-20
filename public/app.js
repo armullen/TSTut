0410227c627cba3176124cd6ab0bfe5ca4d66487
Amber Mullen <121705867+armullen@users.noreply.github.com>
2023-09-20 17:01:33 -0500

    night

--- public/app.js
+++ public/app.js
@@ -1,15 +1,25 @@
 "use strict";
 class Invoice {
     constructor(c, d, a) {
-        this.form = document.querySelector('.new-item-form');
-        this.type = document.querySelector('#type');
-        this.tofrom = document.querySelector('#tofrom');
-        this.details = document.querySelector('#details');
-        this.amount = document.querySelector('#amount');
     }
-    addEventListener() { }
+    format() {
+        return `${this.client} owes $${this.amount} for ${this.details}`;
+    }
 }
-(e) => {
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+let invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+invOne.client = 'yoshi';
+invTwo.amount = 400;
+console.log(invOne, invTwo);
+const form = document.querySelector('.new-item-form');
+const type = document.querySelector('#type');
+const tofrom = document.querySelector('#tofrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+form.addEventListener('submit', (e) => {
     e.preventDefault();
     console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
-};
+});
